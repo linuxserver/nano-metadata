2b0d5f7f95127fa318b4ce1347c841c7e8b37271
thelamer <ryankuba@gmail.com>
2020-06-23 12:39:23 -0700

    hammer out functionality and do initial styling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
 <div id="app">
+  <notifications position="top center"/>
   <div class="heading">
     <h1>Add Metadata to a Transaction</h1>
     <h4>32 characters UTF-8 or 64 HEX (32 Bytes)</h4>
@@ -19,21 +20,29 @@
       <input class="form-control" type="password" placeholder="Seed" v-model="seed">
       <input class="form-control" type="text" placeholder="Transaction Hash" v-model="trans">
       <select class="custom-select mr-sm-2" v-model="net">
+        <option value="" disabled>Select your network</option>
         <option value="live">Live Nano Network</option>
         <option value="lsio">LSIO Nano Network</option>
       </select>
       <div class="container">
         <div class="row">
           <div class="col text-center">
-            <button @click="metadata" class="btn btn-secondary">Add Metadata</button>
+            <button @click="metadata()" class="btn btn-secondary">Add Metadata <font-awesome-icon icon="spinner" v-show="loading !== false"/></button>
           </div>
         </div>
       </div>
     </div>
   </div>
   <div class="container border">
-    <div class="col text-center">
-      <input class="form-control" type="text" v-model="apiurl">
+    <div class="col">
+      <div class="form-group">
+        <label for="apiurl">API URL: <font-awesome-icon @click="copyToClipboard(apiurl)" icon="clone" /></label>
+        <input class="form-control" type="text" id="apiurl" v-model="apiurl">
+      </div>
+      <div class="form-group">
+        <label for="metaurl">Metadata URL: <font-awesome-icon @click="copyToClipboard(metaurl)" icon="clone" /></label>
+        <input class="form-control" type="text" id="metaurl" v-model="metaurl">
+      </div>
     </div>
   </div>
 </div>
@@ -54,7 +63,9 @@ export default {
       seed: '',
       trans: '',
       apiurl: '',
-      net: ''
+      net: '',
+      metaurl: '',
+      loading: false
     }
   },
   watch: {
@@ -69,15 +80,66 @@ export default {
   methods: {
     async metadata() {
       const that = this
-      const hash = blake2sHex(that.trans + that.hex).toUpperCase()
-      const privkey = NanoCurrency.deriveSecretKey(that.seed,0)
-      const pubkey = NanoCurrency.derivePublicKey(privkey)
-      const sig = NanoCurrency.signBlock({ hash: hash, secretKey: privkey })
-      const params = 'trans=' + that.trans + '&data=' + that.hex + '&pub=' + pubkey + '&sig=' + sig + '&net=' + that.net
-      that.apiurl = 'https://api.nanometadata.com/metadata?' + params
-      console.log('https://api.nanometadata.com/metadata?' + params)
+      that.loading = true
+      if (NanoCurrency.checkHash(that.trans) && NanoCurrency.checkSeed(that.seed) && that.hex && that.net) {
+        const response = await fetch('https://www.nanometadata.com/' + that.trans)
+        if (response.ok) {
+          that.$notify({
+            title: 'Error',
+            text: 'Metadata already set',
+            type: 'error'
+          })
+          that.metaurl = 'https://www.nanometadata.com/' +  that.trans
+          that.loading = false
+        } else {
+          const hash = blake2sHex(that.trans + that.hex).toUpperCase()
+          const privkey = NanoCurrency.deriveSecretKey(that.seed,0)
+          const pubkey = NanoCurrency.derivePublicKey(privkey)
+          const sig = NanoCurrency.signBlock({ hash: hash, secretKey: privkey })
+          const params = 'trans=' + that.trans + '&data=' + that.hex + '&pub=' + pubkey + '&sig=' + sig + '&net=' + that.net
+          that.apiurl = 'https://api.nanometadata.com/metadata?' + params
+          const apiresponse = await fetch(that.apiurl)
+          const apicontent = await response.text()
+          if (apiresponse.ok) {
+            that.$notify({
+              title: 'Transaction signed',
+              text: 'Transaction has been signed at nanometadata.com',
+              type: 'success'
+            })
+            that.loading = false
+            that.metaurl = 'https://www.nanometadata.com/' +  that.trans
+          } else {
+            that.$notify({
+              title: 'Cannot sign transaction',
+              text: apicontent,
+              type: 'error'
+            })
+            that.loading = false
+          }
+        }
+      } else {
+        that.$notify({
+          title: 'Error',
+          text: 'Input Parameters invalid',
+          type: 'error'
+        })
+        that.loading = false
+      }
+    },
+    copyToClipboard(text) {
+      let input = document.createElement('textarea');
+      input.innerHTML = text;
+      document.body.appendChild(input);
+      input.select();
+      document.execCommand('copy');
+      document.body.removeChild(input);
+      this.$notify({
+        title: 'Success!',
+        text: 'Copied to clipboard',
+        type: 'success'
+      })
     }
-  },
+  }
 }
 </script>
 
@@ -89,5 +151,11 @@ export default {
 }
 .heading {
   text-align: center;
+}
+@keyframes spinner {
+  to { transform: rotate(360deg); }
+}
+.fa-spinner {
+  animation: spinner 1s linear infinite;
 }
 </style>
